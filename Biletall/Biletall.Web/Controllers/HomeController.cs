b7baf735ecbf8b813f0164f0ca1cb200223cce18
celalpoyraz <44697173+qwerty3858@users.noreply.github.com>
2020-08-24 00:49:08 +0300

    project update

--- Biletall/Biletall.Web/Controllers/HomeController.cs
+++ Biletall/Biletall.Web/Controllers/HomeController.cs
@@ -1,99 +1,85 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Biletall.Web.Models;
-using System.Net;
 using System.Xml;
-using System.IO;
-using System.Text;
-using System.Data;
-using System.Security.Cryptography.Xml;
-using Microsoft.AspNetCore.Server.Kestrel;
-using System.ServiceModel.Channels;
-using System.ServiceModel;
 using ServiceReference1;
-using Biletall.Web.Controllers;
+using Biletall.Web.BusinesLogic;
+using System.Xml.Schema;
+using System.Linq;
 
 namespace Biletall.Web.Controllers
 {
-    public class KaraNokta
-    {
-        public string ID { get; set; }
-        public string SeyahatSehirID { get; set; }
-        public string Bolge { get; set; }
-        public string Ad { get; set; }
-        public string Aciklama { get; set; }
-        public string MerkezMi { get; set; }
-        public string BagliOlduguNoktaID { get; set; }
 
-        }
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
         }
 
-       
-
         public IActionResult Index()
         {
 
-            XmlIsletRequestBody xirb = new XmlIsletRequestBody();
-            XmlDocument xml = new XmlDocument();
-            xml.LoadXml("<Kullanici><Adi>" + "stajyerWS" + "</Adi><Sifre>" + "2324423WSs099"
-+ "</Sifre></Kullanici>");
-            xirb.xmlYetki = xml.DocumentElement;
-
-            XmlDocument xml2 = new XmlDocument();
-            xml2.LoadXml(@"<KaraNoktaGetirKomut/>");
-            xirb.xmlIslem = xml2.DocumentElement;
-
-            var xx = new XmlIsletRequest(xirb);
-
-            var service = new ServiceSoapClient(ServiceSoapClient.EndpointConfiguration.ServiceSoap).XmlIslet(xirb.xmlIslem, xirb.xmlYetki);
-
-            List<KaraNokta> list = new List<KaraNokta>();
-
-            XmlNodeList xnList = service.SelectNodes("/KaraNokta");
-            foreach (XmlNode xn in xnList)
-            {
-                KaraNokta kn = new KaraNokta
-                {
-                    ID = xn["ID"].InnerText,
-                    Ad = xn["Ad"].InnerText,
-                    Aciklama = xn["Aciklama"].InnerText,
-                    BagliOlduguNoktaID = xn["BagliOlduguNoktaID"].InnerText,
-                    Bolge = xn["Bolge"].InnerText,
-                    MerkezMi = xn["MerkezMi"].InnerText,
-                    SeyahatSehirID = xn["SeyahatSehirID"].InnerText
-                };
-                if (kn.MerkezMi == "1")
-                {
-                    list.Add(kn);
-                }
-            }
-            //return data;
-            ViewBag.Cities = list;
-
+            var karaNoktalari = BiletAllService.KaraNoktalariGetir();
+            ViewBag.Cities = karaNoktalari;
             return View();
         }
-
-
         [HttpPost]
         public IActionResult Index(string nereden, string nereye, string tarih)
         {
+            DateTime seferTarihi = new DateTime(Convert.ToInt32(tarih.Split('/')[2]), Convert.ToInt32(tarih.Split('/')[0]), Convert.ToInt32(tarih.Split('/')[1]));
+            var seferler = BiletAllService.SeferleriGetir(nereden, nereye, seferTarihi);
+            if (seferler.Count > 0)
+            {
+                ViewData["Seferler"] = seferler;
+                return RedirectToAction("Seferler", "Home", new { nereden = nereden, nereye = nereye, tarih = seferTarihi });
+            }
+            else
+            {
+                var sonuc = "Sefer bulunamadı!";
+                var karaNoktalari = BiletAllService.KaraNoktalariGetir();
+                ViewBag.Cities = karaNoktalari;
+                ViewBag.Sonuc = sonuc;
+                return View();
+            }
 
-            return View();
         }
 
+        public IActionResult Seferler(string nereden, string nereye, DateTime tarih, bool tariheGoreSirala = false, bool fiyataGoreSırala = false, bool ucluKoltukFiltre = false, bool ikiliKoltukFiltre = false)
+        {
+            ViewBag.Nereden = nereden;
+            ViewBag.Nereye = nereye;
+            ViewBag.Tarih = tarih;
 
+            ViewBag.UcluKoltukFiltre = ucluKoltukFiltre;
+            ViewBag.IkiliKoltukFiltre = ikiliKoltukFiltre;
+            //Seferleri getiren servis
+            var seferler = BiletAllService.SeferleriGetir(nereden, nereye, tarih);
+            if (tariheGoreSirala)
+            {
+                seferler = seferler.OrderBy(n => n.KalkisSaati).ToList();
+            }
+            if (fiyataGoreSırala)
+            {
+                seferler = seferler.OrderBy(n => n.BiletFiyatiInternet).ToList();
+            }
+            if (ucluKoltukFiltre)
+            {
+                seferler = seferler.Where(n => n.OtobusKoltukYerlesimTipi == "2+1").ToList();
+            }
+            if (ikiliKoltukFiltre)
+            {
+                seferler = seferler.Where(n => n.OtobusKoltukYerlesimTipi == "1+1").ToList();
+            }
+            ViewData["Seferler"] = seferler;
+            return View("Seferler");
+
+        }
+        
         public IActionResult Privacy()
         {
             return View();
